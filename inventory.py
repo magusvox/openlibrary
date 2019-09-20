2b56b67f1fddd69f84aaefc0f068aa12da0b4b55
MagusVOX <lurtzrobot@gmail.com>
2019-09-19 23:00:20 -0300

    almost

--- inventory.py
+++ inventory.py
@@ -1,26 +1,31 @@
 def inventory(intx, inty):
-    columns = 5
-    rows = 5
-    x = 5
-    line = []
-    allslots = columns * 1
-    inv = (columns, rows)
+    columns = 3
+    rows = 3
+    x = 3
+    slot = []
     while columns > 0:
         while rows > 0:
-            if inv == (intx, inty):
-                inv = (0, 0)
-                line += inv
-                rows -= 1
-                inv = (columns, rows)
-            else:
-                line += inv
-                rows -= 1
-                inv = (columns, rows)
-        print(line)
-        line = []
+            controw = x
+            y = 0
+            while controw > 0:
+                if rows == intx and columns == inty:
+                    inv = '   Item   '
+                    slot.insert(y, inv)
+                    y += 1
+                    rows -= 1
+                    #inv = (columns, rows)
+                    inv = '##########'
+                    controw -= 1
+                else:
+                    #inv = (columns, rows)
+                    inv = '##########'
+                    slot.insert(y, inv)
+                    y += 1
+                    rows -= 1
+                    controw -= 1
+        print(slot)
+        slot = []
         columns -= 1
         rows = x
-        inv = (columns, rows)
 
-inventory(3, 3)
-
+inventory(2, 2)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-title
+slot.insert(y, inv)
